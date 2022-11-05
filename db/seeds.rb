260f180ceb0bda4900ce7738a1d841747567716e
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-05 23:33:33 +0300

    add dummy data

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+puts "Seeding..."
+
+Movie.create(title: "Black Panther", release_year: 2015)
+Movie.create(title: "Black Widow", release_year: 2020)
+Movie.create(title: "Manhattan", release_year: 2014)
+Movie.create(title: "Django Unchained", release_year: 2004)
+
+puts "Done seeding..."
