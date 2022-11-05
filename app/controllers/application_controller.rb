d546a4c11526637d68e615a003a09ea4cec377c5
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-06 00:00:42 +0300

    add post method

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < Sinatra::Base 
+
+    get '/movies' do 
+        movies = Movie.all
+
+        movies.to_json
+    end
+
+    get '/movies/:id' do 
+        movie = Movie.find(params[:id])
+
+        movie.to_json
+    end
+
+    post '/movies' do 
+        movie = Movie.create(
+            title: params[:title],
+            release_year: params[:release_year]
+        )
+        movie.to_json
+    end
+end
